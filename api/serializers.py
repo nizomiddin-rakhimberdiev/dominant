a32f941d9c5f5848d341d2cf7cf30f565a905b22
Behruz Husanovich <82934751+GornizolCoder-GC@users.noreply.github.com>
2022-11-09 18:57:06 +0500

    news-api-created

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from .models import Service, Order
+from users.models import CustomUser
+from .models import Service, Order, Review
 
 
 class ServiceSerializer(serializers.ModelSerializer):
@@ -8,6 +9,10 @@ class ServiceSerializer(serializers.ModelSerializer):
         model = Service
         fields = ('id', 'name', 'no_action_price', 'action_price', 'created_at')
 
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["id", "first_name", "last_name", "username", 'phone_number', 'profile_pic', 'address']
 
 class OrderSerializer(serializers.ModelSerializer):
     service = ServiceSerializer(read_only=True)
@@ -17,3 +22,11 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ('id', 'phone', 'service', 'service_id', 'order_status', 'offera', 'created_at')
+
+class ReviewSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer(read_only=True)
+
+    user_id = serializers.IntegerField(write_only=True)
+    class Meta:
+        model = Review
+        fields = ('id', 'user', 'user_id', 'comment', 'star', 'created_at')
--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import OrderViewSet, ServiceViewSet
+from .views import OrderViewSet, ServiceViewSet, ReviewViewSet
 
 app_name='api'
 
 router = DefaultRouter()
 router.register('services', ServiceViewSet, basename='service')
 router.register('orders', OrderViewSet, basename='order')
+router.register('reviews', ReviewViewSet, basename='review')
 
 urlpatterns = router.urls
