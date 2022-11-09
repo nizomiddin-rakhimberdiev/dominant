8f880bd66c37e60f0333d0ea781e16ef253e828a
Behruz Husanovich <82934751+GornizolCoder-GC@users.noreply.github.com>
2022-11-09 16:12:12 +0500

    created-users-review

--- users/models.py
+++ users/models.py
@@ -1,16 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
+# from phonenumber_field.modelfields import PhoneNumberField
 
 
 # Create your models here.
 class CustomUser(AbstractUser):
     profile_pic = models.ImageField(default='default_profile_pic.png')
-    phone_number = PhoneNumberField(unique=True)
+    phone_number = models.CharField(max_length=18, unique=True)
     address = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.phone_number
+    # def __str__(self):
+    #     return self.phone_number
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+
